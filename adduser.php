0319c02cf67738fed4053665144b25c56aa1b00d
unorthodoxdaniel <unorthodoxdaniel@gmail.com>
2018-05-08 19:57:49 +0100

    Add User Form

---
+++ adduser.php
@@ -0,0 +1,15 @@
+<html>
+    <head></head>
+    <body>
+        <div>
+            <form>
+            <input type ="text" name = "firstName" placeholder = "FIRST NAME"></input>
+            <input type ="text" name = "lastName" placeholder = "LAST NAME"></input>
+            <input type ="number" name = "phoneNo" placeholder = "PHONE NO"></input>
+            <input type ="textarea" name = "address" placeholder = "ADDRESS"></input>
+            <input type ="text" name = "user-name" placeholder = "USERNAME"></input>
+            <input type ="password" name = "pass-word" placeholder = "PASSWORD"></input>
+            <input type ="password" name = "repeatPassword" placeholder = "REPEAT PASSWORD"></input></form>
+        </div>
+    </body>
+</html>
