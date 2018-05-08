212f31f3467ec2b1c9deef48b928c9e7cc4d8714
unorthodoxdaniel <unorthodoxdaniel@gmail.com>
2018-05-08 19:22:33 +0100

    Login form html

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+    <head> </head>
+    <body> 
+        <div id ="login-box"> LOG IN
+        
+        <form>
+            <input type = "text" name = "username" placeholder ="USERNAME"></input>
+            <input type ="password" name = "password" placeholder = "PASSWORD"></input>
+            <input type = "button" value ="LOGIN"></input>
+        </form>
+        </div>
+    </body>
+</html>
