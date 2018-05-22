55399ecceb9a11b4eb6949cdcf43216758b2c483
unorthodoxdaniel <unorthodoxdaniel@gmail.com>
2018-05-22 21:05:13 +0100

    Database Corrections

--- files/hospital_management.sql
+++ files/hospital_management.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: May 22, 2018 at 06:41 AM
+-- Generation Time: May 22, 2018 at 08:03 PM
 -- Server version: 5.7.19
 -- PHP Version: 5.6.31
 
@@ -25,23 +25,23 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `login`
+-- Table structure for table `login_info`
 --
 
-DROP TABLE IF EXISTS `login`;
-CREATE TABLE IF NOT EXISTS `login` (
+DROP TABLE IF EXISTS `login_info`;
+CREATE TABLE IF NOT EXISTS `login_info` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(30) NOT NULL,
   `pass_word` varchar(10) NOT NULL,
   `staff_type` varchar(30) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `login`
+-- Dumping data for table `login_info`
 --
 
-INSERT INTO `login` (`id`, `username`, `pass_word`, `staff_type`) VALUES
+INSERT INTO `login_info` (`id`, `username`, `pass_word`, `staff_type`) VALUES
 (1, 'admin1', 'admin1', 'admin'),
 (2, 'admin2', 'admin2', 'admin'),
 (3, 'doc1', 'doc1', 'doctor'),
@@ -63,7 +63,7 @@ CREATE TABLE IF NOT EXISTS `patient_bio` (
   `phonenum` varchar(15) NOT NULL,
   `address` varchar(1000) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `patient_bio`
@@ -72,7 +72,10 @@ CREATE TABLE IF NOT EXISTS `patient_bio` (
 INSERT INTO `patient_bio` (`id`, `firstname`, `lastname`, `phonenum`, `address`) VALUES
 (1, 'Christian', 'Daniels', '08011111111', '24, Festac Drive Mile 2'),
 (2, 'Asaph', 'Mic', '08022222222', 'F206, Enijokwu Hall Unilag'),
-(3, 'Daniel', 'Abudu', '08135608364', 'Unilag road, Akoka, Yaba, Lagos');
+(3, 'Daniel', 'Abudu', '08135608364', 'Unilag road, Akoka, Yaba, Lagos'),
+(6, '$firstName', '$lastName', '$phoneNo', '$address'),
+(7, '', '', '08135608364', 'Unilag road, Akoka, Yaba, Lagos'),
+(8, '', '', '08135608364', 'Unilag road, Akoka, Yaba, Lagos');
 
 -- --------------------------------------------------------
 
@@ -107,24 +110,23 @@ CREATE TABLE IF NOT EXISTS `staff_bio` (
   `firstname` varchar(30) NOT NULL,
   `lastname` varchar(30) NOT NULL,
   `phonenumber` varchar(15) NOT NULL,
-  `address` varchar(200) NOT NULL,
+  `homeaddress` varchar(200) NOT NULL,
   `staff_type` varchar(20) NOT NULL,
   `username` varchar(30) NOT NULL,
-  `password` varchar(32) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `staff_bio`
 --
 
-INSERT INTO `staff_bio` (`id`, `firstname`, `lastname`, `phonenumber`, `address`, `staff_type`, `username`, `password`) VALUES
-(1, 'Benjamin ', 'Alamu', '08103374289', 'Lagos', 'admin', 'admin1', 'abcd'),
-(2, 'Daniel', 'Abudu', '08011111111', 'Mariere Hall, Unilag', 'admin', 'admin2', '1234'),
-(3, 'Amaka', 'Ezeoke', '08022222222', 'Fagunwa Hall, Unilag', 'doctor', 'doc1', 'asdf'),
-(4, 'Nifise', 'Oduduwa', '08033333333', 'Somewhere in Lagos', 'doctor', 'doc2', '4567'),
-(5, 'Christian', 'Daniels', '08044444444', 'Jaja Hall, Unilag', 'supportstaff', 'support1', 'jklm'),
-(6, 'Micheal', 'Ejeh', '08055555555', 'Enijokwu Hall, Unilag', 'supportstaff', 'support2', 'uiop');
+INSERT INTO `staff_bio` (`id`, `firstname`, `lastname`, `phonenumber`, `homeaddress`, `staff_type`, `username`) VALUES
+(1, 'Benjamin ', 'Alamu', '08103374289', 'Lagos', 'admin', 'admin1'),
+(2, 'Daniel', 'Abudu', '08011111111', 'Mariere Hall, Unilag', 'admin', 'admin2'),
+(3, 'Amaka', 'Ezeoke', '08022222222', 'Fagunwa Hall, Unilag', 'doctor', 'doc1'),
+(4, 'Nifise', 'Oduduwa', '08033333333', 'Somewhere in Lagos', 'doctor', 'doc2'),
+(5, 'Christian', 'Daniels', '08044444444', 'Jaja Hall, Unilag', 'supportstaff', 'support1'),
+(6, 'Micheal', 'Ejeh', '08055555555', 'Enijokwu Hall, Unilag', 'supportstaff', 'support2');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
