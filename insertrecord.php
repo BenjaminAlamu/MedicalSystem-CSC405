c2ebba6ca8bee74a8b65741b06018dee318272b1
BenjaminAlamu <oluwaseunalamu@gmail.com>
2018-05-23 11:14:03 +0100

    Addeed the insert record page.

--- insertrecord.php
+++ insertrecord.php
@@ -1,8 +1,22 @@
 <?php
+session_start();
 include "inc/database.php";
 
 
 print_r($_POST);
+$patientID = $_POST['patientID'];
+$staffID = $_SESSION['id'];
+$staffname = $_SESSION['staff_name'];
+$patientComplaint = $_POST['patientComplaint'];
+$doctorsNotes = $_POST['doctorNotes'];
+$prescription = $_POST['prescription'];
+$sessionCost = $_POST['price'];
+
+$sql = "INSERT INTO patientrecord (patientID, staffID, staffName, patientComplaint, doctorsNotes, prescription, sessionCost) VALUES ('$patientID','$staffID','$staffname','$patientComplaint','$doctorsNotes', '$prescription', '$sessionCost')";
+echo $sql;
+
+$conn -> exec($sql);
+header('Location:staff.php');
 
 
 ?>
